7543be796137c54c1dd87757a1fbf6a2f11e9063
yshimazu <shimazuyuki7@gmail.com>
2021-09-30 12:34:06 +0900

    norm args

--- srcs/args_check.c
+++ srcs/args_check.c
@@ -2,14 +2,14 @@
 
 int	extension_check(char *filename)
 {
-	char *dot_ptr;
-	
+	char	*dot_ptr;
+
 	dot_ptr = ft_strchr(filename, '.');
-	if(dot_ptr == NULL)
+	if (dot_ptr == NULL)
 		return (-1);
 	else
 	{
-		if(ft_strncmp(dot_ptr, ".bar", 4))
+		if (ft_strncmp(dot_ptr, ".bar", 4))
 			return (-1);
 	}
 	return (0);
@@ -17,12 +17,12 @@ int	extension_check(char *filename)
 
 void	args_check(int ac, char **av)
 {
-	if(ac != 2)
+	if (ac != 2)
 	{
 		printf("You need two args: ./so_long xxx.bar");
 		exit (0);
 	}
-	if(extension_check(av[1]) == -1)
+	if (extension_check(av[1]) == -1)
 	{
 		printf("You need to have xxx.bar file");
 		exit (0);
