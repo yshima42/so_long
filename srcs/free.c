d6faddfd033c61cb22f17059a2b846aa3bb368fa
yshimazu <shimazuyuki7@gmail.com>
2021-09-30 12:35:14 +0900

    norm

--- srcs/free.c
+++ srcs/free.c
@@ -19,19 +19,19 @@ void	free_all_conf(t_conf *conf)
 {
 	if (conf->map.map)
 		conf->map.map = ft_arrayfree(conf->map.map);
-	if(conf->images.empty)
+	if (conf->images.empty)
 		mlx_destroy_image(conf->mlx, conf->images.empty);
-	if(conf->images.wall)
+	if (conf->images.wall)
 		mlx_destroy_image(conf->mlx, conf->images.wall);
-	if(conf->images.collectible)
+	if (conf->images.collectible)
 		mlx_destroy_image(conf->mlx, conf->images.collectible);
-	if(conf->images.exit)
+	if (conf->images.exit)
 		mlx_destroy_image(conf->mlx, conf->images.exit);
-	if(conf->images.player)
+	if (conf->images.player)
 		mlx_destroy_image(conf->mlx, conf->images.player);
-	if(conf->win)
+	if (conf->win)
 		mlx_destroy_window(conf->mlx, conf->win);
-	if(conf->mlx)
+	if (conf->mlx)
 	{
 		mlx_destroy_display(conf->mlx);
 		free(conf->mlx);
