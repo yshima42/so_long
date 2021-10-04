a7fc19e79f20a39f293a6540373a1ef1f65b9b35
yshima42 <shimazuyuki7@gmail.com>
2021-10-04 05:53:12 +0200

    refactoring

--- srcs_bonus/player_action_bonus.c
+++ srcs_bonus/player_action_bonus.c
@@ -27,18 +27,21 @@ void	collect_item(char *a, char *b, t_conf *conf)
 	conf->player.collectibles++;
 }
 
-static void	n_anime_loop(char *img2, t_conf *conf)
+static void	anime_loop(char *img1, char *img2, t_conf *conf)
 {
 	int	count;
 
 	count = 0;
-	while (++count < 10000)
+	mlx_destroy_image(conf->mlx, conf->images.player);
+	conf->images.player = mlx_xpm_file_to_image(conf->mlx, img1,
+			&conf->images.size, &conf->images.size);
+	while (++count < 15000)
 		ani_to_dis(conf->player.pos_y, conf->player.pos_x,
 			conf->map.map, conf);
 	mlx_destroy_image(conf->mlx, conf->images.player);
 	conf->images.player = mlx_xpm_file_to_image(conf->mlx, img2,
 			&conf->images.size, &conf->images.size);
-	while (++count < 20000)
+	while (++count < 30000)
 		ani_to_dis(conf->player.pos_y, conf->player.pos_x,
 			conf->map.map, conf);
 }
@@ -55,10 +58,5 @@ void	game_finish_animation(char *img1, char *img2, t_conf *conf)
 		array_to_screan(conf->map.map, conf);
 	}
 	while (++n_animation < 5)
-	{
-		mlx_destroy_image(conf->mlx, conf->images.player);
-		conf->images.player = mlx_xpm_file_to_image(conf->mlx, img1,
-				&conf->images.size, &conf->images.size);
-		n_anime_loop(img2, conf);
-	}
+		anime_loop(img1, img2, conf);
 }
