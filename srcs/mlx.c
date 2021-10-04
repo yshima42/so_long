6e32c388da7f197ec4877f1cb9cd4824070ac8e0
yshima42 <shimazuyuki7@gmail.com>
2021-10-04 15:23:50 +0200

    manda fix

--- srcs/mlx.c
+++ srcs/mlx.c
@@ -54,20 +54,35 @@ int	key_hook(int keycode, t_conf *conf)
 
 void	desplay_mlx(t_conf *conf)
 {
+	int	size_x;
+	int	size_y;
+	
 	conf->mlx = mlx_init();
 	if (!conf->mlx)
 		error_mlx(conf);
 	chip_set(conf);
-	conf->win = mlx_new_window(conf->mlx, SCREAN_SIZE * (CHIP_SIZE + 1),
-			SCREAN_SIZE * (CHIP_SIZE + 1), "so_long");
+	size_y = (int)conf->map.height * CHIP_SIZE;
+	size_x = (int)conf->map.width * CHIP_SIZE;
+	if (conf->map.height > SCREAN_SIZE)
+		size_y = SCREAN_SIZE * (CHIP_SIZE + 1);
+	if (conf->map.width > SCREAN_SIZE)
+		size_x = SCREAN_SIZE * (CHIP_SIZE + 1);
+	conf->win = mlx_new_window(conf->mlx, size_x, size_y, "so_long");
 	if (!conf->mlx)
 		error_mlx(conf);
 	array_to_screan(conf->map.map, conf);
+}
+
+int	expose(t_conf *conf)
+{
+	array_to_screan(conf->map.map, conf);
+	return(0);
 }
 
 void	hook_loop_mlx(t_conf *conf)
 {
 	mlx_hook(conf->win, 2, 1L << 0, key_hook, conf);
 	mlx_hook(conf->win, 33, 1L << 17, free_all_exit, conf);
+	mlx_hook(conf->win, 12, 1L << 15, expose, conf);
 	mlx_loop(conf->mlx);
 }
